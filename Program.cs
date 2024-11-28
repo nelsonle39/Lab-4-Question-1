6751352567b9bd0ec70b7aac72239ff16f7a0cff
Nelson Le <nelsonle345@gmail.com>
2024-11-27 20:46:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringExtensions_Tester
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "Hello";
+            Console.WriteLine($"{testString} starts with upper? {testString.StartsWithUpper()}");
+
+            testString = "hello";
+            Console.WriteLine($"{testString} starts with upper? {testString.StartsWithUpper()}");
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace StringExtensions_Tester
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
+
+            char firstChar = value[0];
+            return char.IsUpper(firstChar);
+        }
+    }
+}
